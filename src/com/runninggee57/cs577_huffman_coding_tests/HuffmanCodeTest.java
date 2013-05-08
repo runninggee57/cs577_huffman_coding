fdee4719b335de22631682e65fdc29c68692debe
Phil Morley <runninggee57@gmail.com>
2013-05-07 19:55:21 -0500

    Added spitting out the shortest and longest code in a coding to answer one of the questions

--- src/com/runninggee57/cs577_huffman_coding_tests/HuffmanCodeTest.java
+++ src/com/runninggee57/cs577_huffman_coding_tests/HuffmanCodeTest.java
@@ -1,9 +1,11 @@
 package com.runninggee57.cs577_huffman_coding_tests;
 
-import static org.junit.Assert.*;
 import java.io.BufferedWriter;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.Comparator;
+import java.util.Map;
+import java.util.PriorityQueue;
 import java.util.Set;
 import org.junit.Test;
 import com.runninggee57.cs577_huffman_coding.HuffmanCode;
@@ -15,6 +17,36 @@ public class HuffmanCodeTest {
     HuffmanCode hc = new HuffmanCode();
     
     hc.create(1000, HuffmanCode.AGE.OLDEST);
+    
+    PriorityQueue<Map.Entry<String, String>> shortest = new PriorityQueue<Map.Entry<String, String>>(hc.coding.size(), new Comparator<Map.Entry<String, String>>() {
+          public int compare(Map.Entry<String, String> a, Map.Entry<String, String> b) {
+            if (a.getValue().length() < b.getValue().length())
+              return -1;
+            else if (a.getValue().length() == b.getValue().length())
+              return 0;
+            else
+              return 1;
+          }
+        });
+    PriorityQueue<Map.Entry<String, String>> longest = new PriorityQueue<Map.Entry<String, String>>(hc.coding.size(), new Comparator<Map.Entry<String, String>>() {
+      public int compare(Map.Entry<String, String> a, Map.Entry<String, String> b) {
+        if (a.getValue().length() < b.getValue().length())
+          return 1;
+        else if (a.getValue().length() == b.getValue().length())
+          return 0;
+        else
+          return -1;
+      }
+    });
+    for (Map.Entry<String, String> e : hc.coding.entrySet()) {
+      shortest.add(e);
+      longest.add(e);
+    }
+    
+    Map.Entry<String, String> shortCode = shortest.poll();
+    Map.Entry<String, String> longCode = longest.poll();
+    System.out.println("Shortest: " + shortCode.getKey() + " - " + shortCode.getValue());
+    System.out.println("Longest: " + longCode.getKey() + " - " + longCode.getValue());
     
     Set<String> set = hc.curVocabulary.keySet();
     try {
